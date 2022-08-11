02701beabf1b0cd65b91ed357fb6282fe57c750a
Quentin Raillard <qraillard@sitiv.local>
2022-08-11 16:44:39 +0200

    Update import.php

--- import.php
+++ import.php
@@ -212,10 +212,10 @@ function load($insee) {
 						file_put_contents("actes/".$insee."/vu.txt",$t->id."\n",FILE_APPEND | LOCK_EX);
 						//date du jour
 						$today= date("Y-m-d");
-
+						echo "INSERT INTO ".$pref_tab."index_delib VALUES('$insee',$t->id,'$t->date','$nat','$t->number','$t->classification',\"".str_replace("\n",' ',str_replace('"','\"',($t->subject)))."\",\"".substr($list_fich,0,-1)."\",'$today');";
 						// Ajout de l'acte dans la table mysql
 						exe ("INSERT INTO ".$pref_tab."index_delib VALUES('$insee',$t->id,'$t->date','$nat','$t->number','$t->classification',\"".str_replace("\n",' ',str_replace('"','\"',($t->subject)))."\",\"".substr($list_fich,0,-1)."\",'$today');");
-						echo "INSERT INTO ".$pref_tab."index_delib VALUES('$insee',$t->id,'$t->date','$nat','$t->number','$t->classification',\"".str_replace("\n",' ',str_replace('"','\"',($t->subject)))."\",\"".substr($list_fich,0,-1)."\",'$today');";
+
 						if ($nat=="Actes individuels")
 							$mel_delib_ind.="- $t->number ($nat) $t->subject\n";//<br>";
 						else
